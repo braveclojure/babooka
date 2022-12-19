87ce0f090389a9da14babdd1249e630118aecd42
Andres Moreno <amorenompls@gmail.com>
2022-12-18 16:36:27 -0800

    Destructure args for add-entry and comment redundant code

--- project/src/journal/add.clj
+++ project/src/journal/add.clj
@@ -3,18 +3,16 @@
    [journal.utils :as utils]))
 
 (defn add-entry
-  [opts]
+  [{:keys [opts]}]
   (let [entries (utils/read-entries)]
     (spit utils/ENTRIES-LOCATION
           (conj entries
                 (merge {:timestamp (System/currentTimeMillis)} ;; default timestamp
                        opts)))))
 
-
-
-(ns journal.add
-  (:require
-   [journal.utils :as utils]))
+(comment(ns journal.add
+          (:require
+           [journal.utils :as utils])))
 
 (defn -main
   [entry-text]
